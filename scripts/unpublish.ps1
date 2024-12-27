57216a59d8641c01a726da51dda2410cd6e655a4
kayp514 <pa.ka12@outlook.com>
2024-12-27 17:30:15 -0500

    chore: enhance release workflow and unpublish script
    - Update GitHub Actions release workflow to include npm authentication with a scoped package
    - Add NPM authentication step to securely handle npm token
    - Modify unpublish script to include OTP for unpublishing specific package versions

--- scripts/unpublish.ps1
+++ scripts/unpublish.ps1
@@ -2,7 +2,7 @@
 41..0 | ForEach-Object {
     $version = "3.2.$_"
     Write-Host "Attempting to unpublish @tern-secure/nextjs@$version"
-    $result = npm unpublish "@tern-secure/nextjs@$version" --force
+    $result = npm unpublish "@tern-secure/nextjs@$version" --force --otp=325847
     if ($LASTEXITCODE -eq 0) {
         Write-Host "Successfully unpublished version $version" -ForegroundColor Green
     } else {
