0bf255a62cb59bc86c05ea8235c0f2d8f22bea2e
kayp514 <pa.ka12@outlook.com>
2025-02-01 01:09:34 -0500

    chore: Remove useUser hook export from index.d.ts

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -3,7 +3,6 @@ export { loadFireConfig, validateConfig } from './utils/config';
 export { signInWithEmail } from './app-router/client/actions';
 export { TernSecureProvider } from './app-router/client/TernSecureProvider';
 export { useAuth } from './boundary/hooks/useAuth';
-export { useUser } from './boundary/hooks/useUser';
 export { SignIn } from './components/sign-in';
 export { SignOutButton } from './components/sign-out-button';
 export { SignOut } from './components/sign-out';
